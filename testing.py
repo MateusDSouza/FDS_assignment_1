766f64aead1c430f48869ed1868c098ef1c83331
guillaumeteissier <guillaume0teissier@gmail.com>
2020-10-17 12:20:21 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -8,23 +8,15 @@ from skimage import color
 
 def gauss(sigma):
 
-	# probably better code than this for the output of the x vector of integers
-    if sigma < 1:
-    	_3sigma = 3*sigma
-    	ceiling = math.ceil(_3sigma)
-    	floor = math.floor(-(_3sigma)+1)
-    	x = np.array([x for x in range(floor, ceiling)])
-    if sigma >= 1:
-    	x = np.array([x for x in range(-3*sigma, 3*sigma+1)])
-
-    # Applying the function given to get a 1D gaussian. 
-
+    _3sigma = 3*sigma
+    ceiling = math.floor(_3sigma)
+    floor = math.ceil(-_3sigma)
+    x = np.arange(floor, ceiling+1, 1)
     #Link below for the simple explanation.
     # https://homepages.inf.ed.ac.uk/rbf/HIPR2/gsmooth.htm
     Gx = np.array([((1/(math.sqrt(2*math.pi*sigma)))*math.exp(-(i**2)/(2*sigma**2))) for i in x])
 
     return Gx, x
-
 
 def gaussianfilter(img, sigma):
     
@@ -34,16 +26,23 @@ def gaussianfilter(img, sigma):
     y = x
     Gxy =  np.array([((1/((2*math.pi*sigma)))*math.exp(-((i**2)+(j**2))/(2*sigma**2))) for i in x for j in y])
     kernel = np.reshape(Gxy, (len(x),len(x)))
-
-    smooth_img = conv2(kernel, img, mode= "same")
+    smooth_img = conv2(img, kernel, mode= "same")
 
     return smooth_img
 
+def show_image(image, title='Image', cmap_type='gray'):
+	#Display an image using matplotlib
+	plt.imshow(image, cmap = cmap_type)
+	plt.title(title)
+	plt.axis('off')
+	plt.show()
 
-# that s the image
+# that s the image - CHANGE the directory 
 image = plt.imread("C:/Users/Guillaume/Desktop/Fundamentals of data science/Assignment1/Filtering/graf.png")
 # Turn it gray 
 image = color.rgb2gray(image)
+show_image(image)
 
-test = gaussianfilter(image, 0.5)
-
+# a Higher sigma will make it more blurry
+test = gaussianfilter(image, 5)
+show_image(test)
